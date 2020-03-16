8819f56a39b450543ad5673ebaf20905a55de894
thunder775 <rahulmasih61@gmail.com>
2020-03-16 16:44:09 +0530

    da

--- client.js
+++ client.js
@@ -1,8 +1,32 @@
-var style = {
-    base: {
-        color: "#32325d",
-    }
-};
 
-var card = elements.create("card", { style: style });
-card.mount("#card-element");
+document.addEventListener('DOMContentLoaded',()=>{
+    var stripe = Stripe('pk_test_jTcvZUaY0zm2YBTKixbaWZI700OFTYDBaK');
+
+    var elements = stripe.elements();
+    var cardElement = elements.create('card');
+    var cardholderName = document.getElementById('cardholder-name');
+    var cardButton = document.getElementById('card-button');
+    var clientSecret = cardButton.dataset.secret;
+
+    cardButton.addEventListener('click', function(ev) {
+
+        stripe.confirmCardSetup(
+            clientSecret,
+            {
+                payment_method: {
+                    card: cardElement,
+                    billing_details: {
+                        name: cardholderName.value,
+                    },
+                },
+            }
+        ).then(function(result) {
+            if (result.error) {
+                // Display error.message in your UI.
+            } else {
+                // The setup has succeeded. Display a success message.
+            }
+        });
+    });
+    cardElement.mount('#card-element');
+})
